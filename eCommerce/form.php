f9c90dc0c6be24fc2145ad82939ff9eb1cda0893
Rolly Jake Gayo <112569191+mr-yoso@users.noreply.github.com>
2024-02-05 15:05:42 -0500

    a

---
+++ eCommerce/form.php
@@ -0,0 +1,9 @@
+<?php
+if(!isset($_POST))
+?>
+<form action="" method="post">
+    <label for="">Your Name: <input type="text" name="user_name"/></label>
+
+    <input type="submit" value="Submit your name" name="action">
+</form>
+
